4a853254310702bde490b94d35a009a309dfc3e9
Parveen Singh <singh.webdev@gmail.com>
2017-08-14 18:01:00 -0700

    little change

--- app/src/main/java/com/example/pvsingh/githubproject/ItemDetailFragment.java
+++ app/src/main/java/com/example/pvsingh/githubproject/ItemDetailFragment.java
@@ -40,6 +40,7 @@ public class ItemDetailFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        //commituinbh new nothign else
         if (getArguments().containsKey(ARG_ITEM_ID)) {
             // Load the dummy content specified by the fragment
             // arguments. In a real-world scenario, use a Loader
